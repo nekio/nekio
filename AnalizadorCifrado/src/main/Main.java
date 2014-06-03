67cdd4d4ca11e4cb20df582f0380ccafc15c6535
ivan.carrillo.88@gmail.com <ivan.carrillo.88@gmail.com@8237a79a-abb5-6d77-78af-c8b5f61cad0c>
2014-06-03 05:11:18 +0000

    Subida del Proyecto "AnalizadorCifrado"

---
+++ AnalizadorCifrado/src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+/**
+ * 07/Jun/2014
+ * LANIA - MRYSI
+ * Seguridad en Redes
+ * 
+ * @author LCI. Emiliano Anastasio Landa
+ *         eanastasio@veracruz.gob.mx
+ * 
+ * @author ISC. Sinesio Ivan Carrillo Heredia 
+ *         ivan.carrillo@si-ti.com.mx
+ * 
+ */
+
+import analizador.Analizador;
+
+public class Main {
+    public static void main(String[] args) {
+        new Analizador();
+    }
+}
